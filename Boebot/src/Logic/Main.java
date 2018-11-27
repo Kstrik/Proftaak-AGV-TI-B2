16c6e4a640cbf1c5404d4635dabcd44b34512991
Chiem <Chiem.stevens@outlook.com>
2018-11-27 02:53:03 -0900

    Begin met infrared sensor.
    Infrared sensor update toegevoegd.
    Infrared connectie class toegeveogd.
    Main class logica toegevoegd.

--- Boebot/src/Logic/Main.java
+++ Boebot/src/Logic/Main.java
@@ -1,8 +1,22 @@
 package Logic;
 
+import Hardware.ISensor;
+import Hardware.InfraredSensor;
+import Interface.InfraredConnection;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        /*ArrayList<ISensor> sensors = new ArrayList<>();
+        InfraredConnection infraredConnection = new InfraredConnection();
+        sensors.add(new InfraredSensor(infraredConnection));
 
+        while(true) {
+            for(ISensor s : sensors) {
+                s.update();
+            }
+        }*/
     }
 }
