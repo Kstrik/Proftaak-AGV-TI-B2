dd6d9ab445e712b2b512e716156197aecefa7c9c
Lavictus <markb@baaijbreda.nl>
2019-01-05 20:52:22 +0100

    Gui v2.1

---
+++ GUI/BoeBotGUI/src/sample/Main.java
@@ -0,0 +1,13 @@
+package sample;
+
+
+import javafx.application.Platform;
+
+import static javafx.application.Application.launch;
+
+public class Main {
+
+    public static void main(String[] args) {
+        launch(GUI.class);
+    }
+}
